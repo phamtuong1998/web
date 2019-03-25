631b5a3b16446ff47e5df1df347f6817361a593c
Administrator <Administrator@PC019>
2019-03-25 08:56:43 +0700

    Add project files.

---
+++ tuong/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tuong.Startup))]
+namespace tuong
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
