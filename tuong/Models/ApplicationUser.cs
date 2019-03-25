619843b4642975b650efb4e43c2bc65d3f3da1fc
Administrator <Administrator@PC019>
2019-03-25 10:46:30 +0700

    fghfhgf

--- tuong/Models/ApplicationUser.cs
+++ tuong/Models/ApplicationUser.cs
@@ -20,6 +20,8 @@ namespace tuong.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Category { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
